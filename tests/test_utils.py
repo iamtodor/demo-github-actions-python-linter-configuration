22639140985d7babfa57f5b1952ad54e7a7f0b8c
iamtodor <todor.ilya@gmail.com>
2022-09-03 20:39:16 +0200

    add custom multiplication

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,3 +5,9 @@ def test_custom_sum():
     expected_sum = 6
     actual_sum = custom_sum(4, 2)
     assert expected_sum == actual_sum
+
+
+def test_custom_multiplication():
+    expected_result = 6
+    actual_sum = custom_sum(3, 2)
+    assert expected_result == actual_sum
