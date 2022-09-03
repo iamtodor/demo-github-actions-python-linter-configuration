f54a7639540ac55c932e39429895a2de254c578f
iamtodor <todor.ilya@gmail.com>
2022-09-03 20:41:12 +0200

    fix test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,13 @@
-from utils import custom_sum
+from utils import custom_sum, custom_multiplication
 
 
 def test_custom_sum():
-    expected_sum = 6
-    actual_sum = custom_sum(4, 2)
-    assert expected_sum == actual_sum
+    expected_result = 6
+    actual_result = custom_sum(4, 2)
+    assert expected_result == actual_result
 
 
 def test_custom_multiplication():
     expected_result = 6
-    actual_sum = custom_sum(3, 2)
-    assert expected_result == actual_sum
+    actual_result = custom_multiplication(3, 2)
+    assert expected_result == actual_result
